bf96c570b2a57643533aac492ef348f55ebb718c
Search_Dos <recording_sdmg@outlook.com>
2022-12-02 23:40:46 -0600

    Correcion de espacios

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #include <algorithm>
+
 using namespace std;
 
 int main () {
-	int n,k;
+	int n, k;
 	cin >> n >> k;
 	int vector[n];
 	for (int i = 0; i < n; i++) {
@@ -16,7 +17,7 @@ int main () {
 		int l = 0;
 		int r = n - 1;
 		bool ok = false;
-		while (r>=l) {
+		while (r > =l) {
 			int m = (l + r) / 2;
 			if (vector[m] == x){
 				ok = true;
